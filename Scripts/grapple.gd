e539944bb09c736c82d5d41ff85a3a65d53397a4
Alex Giller <alexgiller@proton.me>
2025-08-26 17:22:38 -0400

    shrunk range and strength of grapple
    reduced grapple range from 300 to 200
    and strength from >4000 to 3500

--- Scripts/grapple.gd
+++ Scripts/grapple.gd
@@ -13,17 +13,17 @@ var latchLocation:Vector2
 func _physics_process(delta: float) -> void:
 	var offset=player.position-position
 	if latched and offset.length_squared()>0:
-		var pull=(min(max((offset+player.velocity+(player.get_gravity()*delta)).length()-offset.length(),0)+4000,5000))
+		var pull=3500
 		#print("grapple pull:",pull)
 		player.velocity=player.velocity-(delta*offset/offset.length()*pull)
 		if player.velocity.length_squared()>640000:
 			player.velocity/=(player.velocity.length()/800)
 		#print(player.velocity.length())
-		if offset.length_squared()>160000:
+		if offset.length_squared()>90000:
 			retracting=true
 			latched=false
 	else:
-		if offset.length_squared()>90000:retracting=true
+		if offset.length_squared()>40000:retracting=true
 		if retracting:
 			if offset.length_squared()<1200:queue_free()
 			else:velocity=2000*offset/offset.length()
