ada6d3eb9e90086d49ea2fa35149110155b1515f
Alex Giller <alexgiller@proton.me>
2025-08-30 16:24:57 -0400

    End part
    Mixed jumps
    still working on end flag

--- Scripts/spring.gd
+++ Scripts/spring.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var strength := 1000.0
 
 func _on_body_entered(body: Node2D) -> void:
-	body.velocity=Vector2(sin(rotation)*strength,-cos(rotation)*strength)
+	body.velocity=Vector2(sin(rotation)*strength+(body.velocity.x),-cos(rotation)*strength)
 	#print("spring:",body.velocity,"frame:",Engine.get_physics_frames())
 	$AnimatedSprite2D.play()
 	body.get_node("Jump Sound").play()
